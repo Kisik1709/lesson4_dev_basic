aad1145ef0c84450de21ede8bc7fb6460233903d
Kisik1709 <themegadimasssik1@gmail.com>
2025-04-23 15:15:08 +0700

    Add files via upload

---
+++ lesson4_dev_basic.py
@@ -0,0 +1,27 @@
+from PIL import Image
+
+r_image = Image.open("red.jpg")
+b_image = Image.open("blue.jpg")
+g_image = Image.open("green.jpg")
+
+
+crop_left = (100, 0, r_image.width, r_image.height)
+r_left = r_image.crop(crop_left)
+
+crop_right = (0, 0, 596, r_image.height)
+b_right = b_image.crop(crop_right)
+
+
+crop_mid = (50, 0, 646, r_image.height)
+r_mid = r_image.crop(crop_mid)
+b_mid = b_image.crop(crop_mid)
+g_mid = g_image.crop(crop_mid)
+
+r_imp = Image.blend(r_left, r_mid, 0.5)
+b_imp = Image.blend(b_right, b_mid, 0.5)
+
+art_monro = Image.merge("RGB", (r_imp, g_mid, b_imp))
+art_monro.save("art_monro.jpg")
+
+art_monro.thumbnail((80, 80))
+art_monro.save("art_monro_icon.jpg")
